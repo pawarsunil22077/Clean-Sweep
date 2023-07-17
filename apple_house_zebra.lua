9c556dcc9ef02500b7cdd317d3fb78fdc087a4a2
pawarsunil22077 <136634901+pawarsunil22077@users.noreply.github.com>
2023-07-17 13:14:33 +0700

    Improve code reusability

---
+++ apple_house_zebra.lua
@@ -0,0 +1,157 @@
+-- "Clean Sweep" in Lua
+
+-- Create a function to clean up the living room.
+
+function clean_livingroom()
+  -- Get the living room area ready 
+  vacuum_floor()
+  dust_shelves()
+  dust_furniture()
+  remove_garbage()
+ 
+  -- Clean up the windows
+  clean_windows()
+ 
+  -- Put away all the toys
+  put_away_toys()
+ 
+  -- Make the sofa and chairs look nice
+  fluff_pillows()
+  fold_blankets()
+ 
+  -- Finish up
+  mop_floor()
+ end
+ 
+-- Create a function to vacuum the floor
+ 
+function vacuum_floor()
+  -- Move all the furniture
+  move_chairs()
+  move_sofa()
+ 
+  -- Plug in the vacuum cleaner
+  plug_in_vacuum()
+ 
+  -- Start vacuuming
+  start_vacuuming()
+ 
+  -- Empty the vacuum cleaner
+  empty_vacuum()
+ 
+  -- Unplug the vacuum cleaner
+  unplug_vacuum()
+ end
+ 
+-- Create a function to dust the shelves
+ 
+function dust_shelves()
+  -- Get the dusting cloth
+  get_dusting_cloth()
+ 
+  -- Take down the items on the shelves
+  remove_items()
+ 
+  -- Dust the shelves
+  start_dusting()
+ 
+  -- Replace the items
+  replace_items()
+ end
+ 
+-- Create a function to dust the furniture
+ 
+function dust_furniture()
+  -- Get the dusting cloth
+  get_dusting_cloth()
+ 
+  -- Dust the furniture
+  start_dusting()
+ end
+ 
+-- Create a function to remove garbage
+ 
+function remove_garbage()
+  -- Put on cleaning gloves
+  put_on_gloves()
+ 
+  -- Collect all the garbage
+  collect_garbage()
+ 
+  -- Empty the garbage bags
+  empty_garbage_bags()
+ end
+
+-- Create a function to clean windows 
+
+function clean_windows()
+  -- Get the window cleaning supplies
+  get_cleaning_supplies()
+ 
+  -- Wash the inside of the windows
+  wash_inside()
+ 
+  -- Wash the outside of the windows
+  wash_outside()
+ 
+  -- Dry the windows
+  dry_windows()
+ end
+
+-- Create a function to put away toys
+
+function put_away_toys()
+  -- Pick up the toys
+  pick_up_toys()
+ 
+  -- Sort the toys
+  sort_toys()
+ 
+  -- Put away the toys
+  put_away_toys()
+ end
+ 
+-- Create a function to fluff pillows 
+
+function fluff_pillows()
+  -- Get the pillows
+  get_pillows()
+ 
+  -- Fluff the pillows
+  fluff_pillows()
+ 
+  -- Place the pillows back
+  place_pillows()
+ end
+ 
+-- Create a function to fold blankets 
+
+function fold_blankets()
+  -- Get the blankets
+  get_blankets()
+ 
+  -- Fold the blankets
+  fold_blankets()
+ 
+  -- Place the blankets back
+  place_blankets()
+ end
+
+-- Create a function to mop the floors
+
+function mop_floor()
+  -- Get the mop and bucket
+  get_mop_bucket()
+ 
+  -- Fill the bucket with water
+  fill_bucket()
+ 
+  -- Add cleaning solution
+  add_solution()
+ 
+  -- Start mopping
+  start_mopping()
+ 
+  -- Empty the bucket
+  empty_bucket()
+ end
